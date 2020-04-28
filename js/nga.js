69fe978d2fefb3886b54a641caa3e2867e135d53
M1nG <285635652@qq.com>
2020-04-28 09:57:21 +0800

    新增手势翻页 部分代码重构

--- js/nga.js
+++ js/nga.js
@@ -1,15 +1,4 @@
 ﻿nga = (function () {
-    /**
-     * 简化栏目名字
-     */
-    function simplifyAD() {
-        $(".c3>a").each(function () {
-            $(this).html($(this).text());
-        });
-        $(".c4>span").each(function () {
-            $(this).html($(this).text());
-        });
-    }
 
     /**
      * 主题 版面默认隐藏
@@ -18,17 +7,22 @@
     function topics() {
         let tip = "<a class='showMore' showId ='#toppedtopic' >[公告]</a>",
             subTip = "<a class='showMore' showId ='#sub_forums'>[子版面]</a>";
-        $("#toptopics >div> h3").append(tip).append(subTip);
-        $(".showMore").toggle(function () {
-            this.display($(this).attr("showId"),true);
-        },function () {
-            this.display($(this).attr("showId"),false);
+        let tipArray = [tip, subTip];
+        tipArray.forEach(function (value) {
+            $("#toptopics >div> h3").append(value);
+            $(".showMore").toggle(function () {
+                display($($(this).attr("showId")), true);
+            }, function () {
+                display($($(this).attr("showId")), false);
+            });
         });
 
-        function display(data,status) {
-            $(id).css("cssText", status?"display:block !important":"display:none !important");
+        function display(data, status) {
+            data.css("cssText", status ? "display:block !important" : "display:none !important");
         }
+
     }
+
 
     /**
      * 简化头像
@@ -36,7 +30,8 @@
     function simplifyAv() {
         $(".posterinfo").each(function () {
             if ($(this).has("img").length === 1) {
-                $(this).find("div:first").append("<a href='javascript:void' class='small_colored_text_btn stxt block_txt_c0 vertmod'>头像</a>");
+                $(this).find("div:first").
+                append("<a href='javascript:void' class='small_colored_text_btn stxt block_txt_c0 vertmod'>头像</a>");
             }
         })
     }
@@ -55,18 +50,29 @@
      * 添加返回顶部标示
      */
     function addReturnTop() {
-        let temp = document.createElement('img');
-        temp.setAttribute("id", "returnTop");
-        temp.src = chrome.extension.getURL("img/top.png");
-        document.body.appendChild(temp);
-        $("#returnTop").on("click", () => {
+        let returnTop = document.createElement('img');
+        returnTop.setAttribute("id", "returnTop");
+        returnTop.src = chrome.extension.getURL("img/top.png");
+        document.body.appendChild(returnTop);
+        $(returnTop).on("click", () => {
             document.scrollingElement.scrollTop = 0;
         })
     }
 
-    addReturnTop();
-    simplifyAD();
-    topics();
-    simplifyAv();
-    injectJs('js/inject/hotKey.js');
+    /**
+     * 初始化
+     */
+    function init() {
+        addReturnTop();
+        topics();
+        simplifyAv();
+        injectJs('js/inject/hotKey.js');
+    }
+
+    return {
+        init: init
+    }
+
 })();
+
+nga.init();
